e2f99cdb0387e8310b7966aa9d58f0912612a3d7
Rachid Biddi <b7hr4m83qq95-student@anonymous-codio.com>
2023-02-21 09:02:53 +0000

    final

--- com/hemebiotech/analytics/Main.java
+++ com/hemebiotech/analytics/Main.java
@@ -16,26 +16,18 @@ public class Main {
   */
   public static void main(String[] args) {
 
-    // Instancer objet ISymptomReader
     ISymptomReader reader = new ReadSymptomDataFromFile("symptoms.txt");
 
-    // Instancer objet ISymptomWriter
     ISymptomWriter writer = new WriteSymptomDataToFile();
 
-
-    // Instancer objet AnalyticsCounter
     AnalyticsCounter analyticsCounter = new AnalyticsCounter(reader, writer);
 
-    // Récupération de la liste des symptômes
     List<String> symptoms = analyticsCounter.getSymptoms();
 
-    // Comptage des occurrences de chaque symptôme
     Map<String, Integer> countedSymptoms = analyticsCounter.countSymptoms(symptoms);
 
-    // Tri de la liste de symptômes et d'occurrences par ordre alphabétique
     Map<String, Integer> sortedSymptoms = analyticsCounter.sortSymptoms(countedSymptoms);
 
-    // Écriture des résultats dans le fichier de sortie
     analyticsCounter.writeSymptoms(sortedSymptoms); 
   }
 }
